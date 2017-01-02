154cecb88b03ab68c4cc19b000e69a73a05e8fa6
relCORE <relcore@gamerarmy.com>
2017-01-02 12:45:22 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 # prime_prestudy_05
-var avengersMovie = {
-	
+var avengersMovie = {	
 name : "Avengers",	
 runtime : 143,
-releaseYear : 2012
-,
-director : "Joss Whedon"
-};
+releaseYear : 2012,
+director : "Joss Whedon"};
 
 console.log(avengersMovie);
